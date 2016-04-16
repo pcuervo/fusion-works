ed2f8dc97a490f504891cdde3e615172699218d9
arisbeth <arisbeth.hg@gmail.com>
2016-04-15 19:15:59 -0500

    pinnacle, eps

--- themes/pinnacle_premium/page-erp.php
+++ themes/pinnacle_premium/page-erp.php
@@ -12,7 +12,7 @@ Template Name: pinnacle_premium
 	<section class="[ container ][ what-is ]">
 		<article class="[ row ]">
 			<div class="[ col-sm-4 ][ margin-bottom--xlarge ][ text-center ]">
-				<img data-wow-offset="500" class="[ wow bounceInDown rollIn ][ img-responsive img-centered ]" src="<?php echo THEMEPATH; ?>assets/img/what-is-erp.png" alt="">
+				<img class="[ wow bounceInDown rollIn ][ img-responsive img-centered ]" src="<?php echo THEMEPATH; ?>assets/img/what-is-erp.png" alt="">
 				<img class="[ wow rollIn ][ img-responsive img-centered ][ width-50 ][ hidden-xs ]" src="<?php echo THEMEPATH; ?>assets/img/3-cogs.png" alt="">
 				<img class="[ wow slideInLeft ][ img-responsive img-centered ][ margin-left--xlarge ][ pull-right ][ hidden-xs ]" src="<?php echo THEMEPATH; ?>assets/img/arrow-down-right.png" alt="">
 			</div>
@@ -88,26 +88,24 @@ Template Name: pinnacle_premium
 			<div class="[ row ][ margin-bottom ]">
 				<div data-wow-delay="1000ms" class="[ col-xs-12 ][ margin-bottom--xlarge ][ wow slideInRight ]">
 					<p class="[ text-large inline text-uppercase text-semi-bold ][ color-primary background-primary-20 ][ padding ]">Our Qualifications</p>
+					<img class="[ wow rotateIn ][ img-responsive ][ absolute right-0 ]" src="<?php echo THEMEPATH; ?>assets/img/3-cogs.png">
 				</div>
 			</div>
 			<div class="[ row ]">
-				<div class="[ wow bounceInRight ][ width-30 ][ pull-right ]">
-					<img class="[ wow rotateIn ][" src="<?php echo THEMEPATH; ?>assets/img/3-cogs.png">
-				</div>
-				<div class="[[ col-sm-8 ] margin-bottom--xlarge ][ wow slideInrRight ]">
+				<div class="[ col-sm-8 ] margin-bottom ][ wow slideInrRight ]">
 					<img class="[ wow rollIn ]" src="<?php echo THEMEPATH; ?>assets/img/key-busines.png" alt="">
-					<p class="[ inline-block  ][ lead ][ color-secondary ][ border-dashed-primary border-radius-10 no-border-left ][ padding ]">Key business metrics are available in real-time</p>
+					<p class="[ inline-block ][ lead ][ color-secondary ][ border-dashed-primary border-radius-10 no-border-left ][ padding ]">Key business metrics are available in real-time</p>
 				</div>
-				<div class="[ col-sm-7 col-sm-offset-2 ][ margin-bottom--xlarge ][ wow slideInLeft ]">
+				<div class="[ col-sm-7 col-sm-offset-2 ][ margin-bottom ][ wow slideInLeft ]">
 					<img class="[ wow rollIn ]" src="<?php echo THEMEPATH; ?>assets/img/running.png" alt="">
 					<p class="[ inline-block width-60 ][ lead ][ color-secondary ][ border-dashed-primary border-radius-10 no-border-left ][ padding ]">Running status reports instantly</p>
 				</div>
-				<div class="[ col-sm-6 col-sm-offset-4  ][ margin-bottom--xlarge ][ wow slideInRight ]">
+				<div class="[ col-sm-6 col-sm-offset-4  ][ margin-bottom ][ wow slideInRight ]">
 					<img class="[ wow rollIn ]" src="<?php echo THEMEPATH; ?>assets/img/streamline.png" alt="">
 					<p class="[ inline-block ][ width-90 ][ lead ][ color-secondary ][ border-dashed-primary border-radius-10 no-border-left ][ padding ]">Streamline operations and improve productivity</p>
 				</div>
-					<img class="[ wow rotateIn ][ pull-left ]" src="<?php echo THEMEPATH; ?>assets/img/3cogs.png">
-				<div class="[ col-sm-6 ][ margin-bottom--xlarge ][ pull-right ][ wow slideInLeft ]">
+				<img class="[ wow rotateIn ][ pull-left ][ img-responsive ]" src="<?php echo THEMEPATH; ?>assets/img/3cogs.png">
+				<div class="[ col-sm-6 ][ margin-bottom ][ pull-right ][ wow slideInLeft ]">
 					<img class="[ wow rollIn ]" src="<?php echo THEMEPATH; ?>assets/img/improve.png" alt="">
 					<p class="[ inline-block width-70 ][ lead ][ color-secondary ][ border-dashed-primary border-radius-10 no-border-left ][ padding ]">Improve accuracy of production costing and forecasting.</p>
 				</div>
@@ -116,28 +114,28 @@ Template Name: pinnacle_premium
 	</section>
 
 
-	<section class="[ background-secondary ][ padding--xlarge ]">
+	<section class="[ background-secondary-60 ][ padding--xlarge ]">
 		<article class="[ container ]">
 			<div class="[ row ]">
 				<div data-wow-delay="1000ms" class="[ col-xs-12 ][ margin-bottom--xlarge ][ wow slideInLeft ]">
-					<p class="[ text-large inline text-uppercase text-semi-bold ][ color-primary background-primary-20 ][ padding ]">Our Qualifications</p>
+					<p class="[ text-large inline text-uppercase text-semi-bold ][ color-primary background-light-60 ][ padding ]">Our Qualifications</p>
 				</div>
 			</div>
 			<div class="[ row ]">
 				<div class="[ col-sm-4 ][ margin-bottom--xlarge ][ wow pulse ]">
 					<img class="[ img-responsive img-centered ][ width-90 ]" src="<?php echo THEMEPATH; ?>assets/img/our-qualifications.png" alt="">
 				</div>
-				<div data-wow-delay="1200ms" class="[ col-sm-8 ][ margin-bottom--xlarge ][ wow slideInRight ]">
-					<p class="[ h3 ][ color-light ]">Fusionworks Inc. is an expert source of</p>
+				<div class="[ col-sm-8 ][ margin-bottom--xlarge ][ wow slideInRight ]">
+					<p class="[ text-large ][ color-light ]">Fusionworks Inc. is an expert source of</p>
 				</div>
-				<div data-wow-delay="1700ms" class="[ col-sm-4 ][ margin-bottom--xlarge ][ wow fadeInDown ]">
+				<div class="[ col-sm-4 ][ margin-bottom--xlarge ][ wow fadeInDown ]">
 					<img class="[ img-responsive img-centered ][ width-90 ]" src="<?php echo THEMEPATH; ?>assets/img/microsoft-logo.png" alt="">
 				</div>
-				<div data-wow-delay="1700ms" class="[ col-sm-4 ][ margin-bottom--xlarge ][ wow fadeInDown ]">
+				<div class="[ col-sm-4 ][ margin-bottom--xlarge ][ wow fadeInDown ]">
 					<img class="[ img-responsive img-centered ][ width-90 ]" src="<?php echo THEMEPATH; ?>assets/img/oracle-logo.png" alt="">
 				</div>
-				<div data-wow-delay="1200ms" class="[ col-sm-8 ][ margin-bottom--xlarge ][ wow slideInRight ]">
-					<p class="[ h3 ][ color-light ]">ERP Solutions with over 30 <br>successful implementations</p>
+				<div data-wow-offset="10" class="[ col-sm-8 ][ margin-bottom--xlarge ][ wow slideInRight ]">
+					<p class="[ text-large ][ color-light ]">ERP Solutions with over 30 <br>successful implementations</p>
 				</div>
 			</div>
 		</article>
