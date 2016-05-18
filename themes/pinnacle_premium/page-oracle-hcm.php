27f8ec336ea9bbd35cfa54181434beaae8978ba2
Nayeli-Jordn <nayeli.jordan16@gmail.com>
2016-05-17 22:37:23 -0500

    primera parte infografia oracle hcm

--- themes/pinnacle_premium/page-oracle-hcm.php
+++ themes/pinnacle_premium/page-oracle-hcm.php
@@ -6,9 +6,149 @@ Template Name: pinnacle_premium
 
 <?php get_header(); ?>
 <?php get_template_part('templates/page', 'header'); ?>
-	<section class="[ text-center ][ background-gray ][ padding-top-bottom--large ]">
-		<img class="[ wow fadeInLeft ][ margin-bottom ]" src="<?php echo THEMEPATH; ?>assets/img/biherramientas.png" alt="Página en proceso">
-		<h2 class="[ wow bounceInDown ][ color-primary ][ text-uppercase ][ margin-bottom--large ]"><strong>Page in progress</strong></h2>
+	<section class="[ container ][ background-secondary ]">
+			<h2 class="[ text-uppercase ][ color-primary ][ margin-bottom--xlarge ][ font-weight--600 ][ wow bounceInLeft ]">Experience</h2>
+		<div class="[ row ]">
+
+		</div>
+		<div class="[ row ]">
+			<div class="[ col-ss-12 col-sm-3 ]">
+				<div class="[ border-primary--info ][ padding--small ][ wow bounceInLeft ]">
+					<div class="[ background-primary--xlight ][ padding ]">
+						<p class="[ color-light ][ text-bold ][ font-size--xlarge ][ no-margin ]">+ 13</p>
+						<p class="[ font-weight--600 ][ no-margin ][ color-primary ][ padding-top ]">Yers</p>
+					</div>
+				</div>
+			</div>
+			<div class="[ col-ss-12 col-sm-3 ]">
+				<div class="[ border-primary--info ][ padding--small ][ wow bounceInLeft ]">
+					<div class="[ background-primary--xlight ][ padding ]">
+						<p class="[ color-light ][ text-bold ][ font-size--xlarge ][ no-margin ]">+ 13</p>
+						<p class="[ font-weight--600 ][ no-margin ][ color-light ][ padding-top ]">Yers</p>
+					</div>
+				</div>
+			</div>
+			<div class="[ col-ss-12 col-sm-3 ]">
+				<div class="[ border-primary--info ][ padding--small ][ wow bounceInLeft ]">
+					<div class="[ background-primary--xlight ][ padding ]">
+						<p class="[ color-light ][ text-bold ][ font-size--xlarge ][ no-margin ]">+ 13</p>
+						<p class="[ font-weight--600 ][ no-margin ][ color-light ][ padding-top ]">Yers</p>
+					</div>
+				</div>
+			</div>
+			<div class="[ col-ss-12 col-sm-3 ]">
+				<div class="[ border-primary--info ][ padding--small ][ wow bounceInLeft ]">
+					<div class="[ background-primary--xlight ][ padding ]">
+						<p class="[ color-light ][ text-bold ][ font-size--xlarge ][ no-margin ]">+ 13</p>
+						<p class="[ font-weight--600 ][ no-margin ][ color-light ][ padding-top ]">Yers</p>
+					</div>
+				</div>
+			</div>
+		</div>
+
+	</section>
+	<section class="[ container ]">
+		<div class="[ row ]">
+			<div class="[ col-ss-12 col-sm-6 ]">
+				<h2 class="[ text-uppercase ][ color-light background-primary ][ font-weight--600 ][ wow bounceInLeft ]">Experience</h2>
+				<p class="[ color-secondary ]">Complete HR and talent suite, that support the entire employee cycle from hire to retire.</p>
+				<div class="[ background-primary ]">
+					<p>Incorporates global, social, and mobile functionality, making it a simple to use tool, with value added insight for best decision making.</p>
+				</div>
+				<div class="[ border-bottom--primary--light--dotted padding-bottom--medium ]">
+					<img class="[ inline-block middle ][ width-20 ][ wow rotateIn ]" src="<?php echo THEMEPATH; ?>assets/img/by-k4.png" alt="key fuctionalities">
+					<p class="[ inline-block middle ][ width-70 ][ background-primary--xlight padding-top-bottom padding--sides ][ text-bold ][ no-margin-bottom ][ color-secondary ][ wow bounceInRight ]">Compliance with state and federal regulations.</p>
+				</div>
+				<div class="[ border-bottom--primary--light--dotted padding-bottom--medium ]">
+					<img class="[ inline-block middle ][ width-20 ][ wow rotateIn ]" src="<?php echo THEMEPATH; ?>assets/img/by-k4.png" alt="key fuctionalities">
+					<p class="[ inline-block middle ][ width-70 ][ background-primary--xlight padding-top-bottom padding--sides ][ text-bold ][ no-margin-bottom ][ color-secondary ][ wow bounceInRight ]">Choose one module or the entire suite</p>
+				</div>
+				<div class="[ border-bottom--primary--light--dotted padding-bottom--medium ]">
+					<img class="[ inline-block middle ][ width-20 ][ wow rotateIn ]" src="<?php echo THEMEPATH; ?>assets/img/by-k4.png" alt="key fuctionalities">
+					<p class="[ inline-block middle ][ width-70 ][ background-primary--xlight padding-top-bottom padding--sides ][ text-bold ][ no-margin-bottom ][ color-secondary ][ wow bounceInRight ]">Co-existence with HR systems already in place</p>
+				</div>
+				<div class="[ border-bottom--primary--light--dotted padding-bottom--medium ]">
+					<img class="[ inline-block middle ][ width-20 ][ wow rotateIn ]" src="<?php echo THEMEPATH; ?>assets/img/by-k4.png" alt="key fuctionalities">
+					<p class="[ inline-block middle ][ width-70 ][ background-primary--xlight padding-top-bottom padding--sides ][ text-bold ][ no-margin-bottom ][ color-secondary ][ wow bounceInRight ]">Ease and speed of cloud delivery, with secure and flexible cloud services</p>
+				</div>
+				<div class="[ border-bottom--primary--light--dotted padding-bottom--medium ]">
+					<img class="[ inline-block middle ][ width-20 ][ wow rotateIn ]" src="<?php echo THEMEPATH; ?>assets/img/by-k4.png" alt="key fuctionalities">
+					<p class="[ inline-block middle ][ width-70 ][ background-primary--xlight padding-top-bottom padding--sides ][ text-bold ][ no-margin-bottom ][ color-secondary ][ wow bounceInRight ]">For small business or multi-national enterprises, of any industry</p>
+				</div>
+				<div class="[ background-secondary ]">
+					<h2 class="[ text-uppercase ][ color-light ][ margin-bottom--xlarge ][ font-weight--600 ][ wow bounceInLeft ]">Enabling the employee</h2>
+					<div class="[ row ]">
+						<div class="[ col-ss-12 col-xs-4 ]">
+							<img class="[ inline-block middle ][ width-20 ][ wow rotateIn ]" src="<?php echo THEMEPATH; ?>assets/img/by-k4.png" alt="key fuctionalities">
+						</div>
+						<div class="[ col-ss-12 col-xs-4 ]">
+							<img class="[ inline-block middle ][ width-20 ][ wow rotateIn ]" src="<?php echo THEMEPATH; ?>assets/img/by-k4.png" alt="key fuctionalities">
+						</div>
+						<div class="[ col-ss-12 col-xs-4 ]">
+							<img class="[ inline-block middle ][ width-20 ][ wow rotateIn ]" src="<?php echo THEMEPATH; ?>assets/img/by-k4.png" alt="key fuctionalities">
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="[ col-ss-12 col-sm-6 ]">
+				<h2 class="[ text-uppercase ][ color-light ][ margin-bottom--xlarge ][ font-weight--600 ][ wow bounceInLeft ]">Key functionalities</h2>
+				<div class="[ row ]">
+					<div class="[ col-ss-12 col-xs-6 col-sm-3 ]">
+						<img class="[ inline-block middle ][ width-20 ][ wow rotateIn ]" src="<?php echo THEMEPATH; ?>assets/img/by-k4.png" alt="key fuctionalities">
+						<p>Global</p>
+					</div>
+					<div class="[ col-ss-12 col-xs-6 col-sm-3 ]">
+						<img class="[ inline-block middle ][ width-20 ][ wow rotateIn ]" src="<?php echo THEMEPATH; ?>assets/img/by-k4.png" alt="key fuctionalities">
+						<p>Native mobile applications</p>
+					</div>
+					<div class="[ col-ss-12 col-xs-6 col-sm-3 ]">
+						<img class="[ inline-block middle ][ width-20 ][ wow rotateIn ]" src="<?php echo THEMEPATH; ?>assets/img/by-k4.png" alt="key fuctionalities">
+						<p>Social platform for enterprise collaboration</p>
+					</div>
+					<div class="[ col-ss-12 col-xs-6 col-sm-3 ]">
+						<img class="[ inline-block middle ][ width-20 ][ wow rotateIn ]" src="<?php echo THEMEPATH; ?>assets/img/by-k4.png" alt="key fuctionalities">
+						<p>Talent Acquisition</p>
+					</div>
+					<div class="[ col-ss-12 col-xs-6 col-sm-3 ]">
+						<img class="[ inline-block middle ][ width-20 ][ wow rotateIn ]" src="<?php echo THEMEPATH; ?>assets/img/by-k4.png" alt="key fuctionalities">
+						<p>On boarding</p>
+					</div>
+					<div class="[ col-ss-12 col-xs-6 col-sm-3 ]">
+						<img class="[ inline-block middle ][ width-20 ][ wow rotateIn ]" src="<?php echo THEMEPATH; ?>assets/img/by-k4.png" alt="key fuctionalities">
+						<p>Compensation Management</p>
+					</div>
+					<div class="[ col-ss-12 col-xs-6 col-sm-3 ]">
+						<img class="[ inline-block middle ][ width-20 ][ wow rotateIn ]" src="<?php echo THEMEPATH; ?>assets/img/by-k4.png" alt="key fuctionalities">
+						<p>Benefits enrollment</p>
+					</div>
+					<div class="[ col-ss-12 col-xs-6 col-sm-3 ]">
+						<img class="[ inline-block middle ][ width-20 ][ wow rotateIn ]" src="<?php echo THEMEPATH; ?>assets/img/by-k4.png" alt="key fuctionalities">
+						<p></p>
+					</div>
+					<div class="[ col-ss-12 col-xs-6 col-sm-3 ]">
+						<img class="[ inline-block middle ][ width-20 ][ wow rotateIn ]" src="<?php echo THEMEPATH; ?>assets/img/by-k4.png" alt="key fuctionalities">
+						<p></p>
+					</div>
+					<div class="[ col-ss-12 col-xs-6 col-sm-3 ]">
+						<img class="[ inline-block middle ][ width-20 ][ wow rotateIn ]" src="<?php echo THEMEPATH; ?>assets/img/by-k4.png" alt="key fuctionalities">
+						<p></p>
+					</div>
+					<div class="[ col-ss-12 col-xs-6 col-sm-3 ]">
+						<img class="[ inline-block middle ][ width-20 ][ wow rotateIn ]" src="<?php echo THEMEPATH; ?>assets/img/by-k4.png" alt="key fuctionalities">
+						<p></p>
+					</div>
+					<div class="[ col-ss-12 col-xs-6 col-sm-3 ]">
+
+					</div>
+					<div class="[ col-ss-12 col-xs-6 col-sm-3 ]">
+
+					</div>
+
+				</div>
+			</div>
+		</div>
+
+
 	</section>
 
 
